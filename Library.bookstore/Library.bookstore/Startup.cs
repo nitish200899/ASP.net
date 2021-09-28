ac0ff8e6b2e8487e8b87615b924c5527eb17a4e0
nitishkesarwani01 <nitish.kesarwani01@nagarro.com>
2021-09-29 00:28:49 +0530

    Created Controller

--- Library.bookstore/Library.bookstore/Startup.cs
+++ Library.bookstore/Library.bookstore/Startup.cs
@@ -16,6 +16,7 @@ namespace Library.bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,14 @@ namespace Library.bookstore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!!");
-                });
-            });
+              app.UseRouting();
+
+             app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapDefaultControllerRoute();
+             });
+            
         }
     }
 }
